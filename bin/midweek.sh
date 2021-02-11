9f8dc7477e6afa3b5a0242fb8e35e50abae19973
Tyrel Souza <tyrelsouza@gmail.com>
2021-02-11 11:32:39 -0500

    ignore

--- bin/midweek.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-open https://zoom.us/j/605629105 
--- bin/watercooler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-day=$(date +"%u")
-if ((day == 5)); then
-  open https://zoom.us/j/476328355
-else
-  open https://zoom.us/j/91489699692?pwd=YTUwbjVBU1Fobklvc05XZ0pUaEwzQT09
-fi
-
