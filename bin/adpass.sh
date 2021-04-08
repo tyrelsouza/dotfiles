a08c0c5223e82795836f6b13575db723556689e8
Tyrel Souza <tyrelsouza@gmail.com>
2021-04-08 15:41:05 -0400

    Remove Addgene stuff

--- bin/adpass.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-smbpasswd -r winsrv2.addgene.org -U tyrel
--- bin/resolv_work.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp /etc/resolv.conf /home/tyrelsouza/prev_resolv.conf
-
-echo > /etc/resolv.conf
-echo "nameserver 10.0.2.22" >> /etc/resolv.conf
-echo "search addgene.org" >> /etc/resolv.conf
