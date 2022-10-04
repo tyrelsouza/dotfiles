1d0f382141c47ab28a1495ef614e95ff0df9fad4
Tyrel Souza <tyrel.souza@everquote.com>
2022-10-04 15:48:41 -0400

    cleanup

--- bin/datecalc.py
+++ bin/datecalc.py
@@ -3,4 +3,4 @@
 from datetime import datetime
 
 if __name__ == "__main__":
-    print(f"Today is March {(datetime.today() - datetime(2020,2,29)).days}")
+    print(f"I am {(datetime.today() - datetime(1988,3,3)).days} days old")
--- bin/harden.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://github.com/drduh/macOS-Security-and-Privacy-Guide
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
-cd ~/bin
-git clone git@github.com:karek314/macOS-home-call-drop.git
-cd macOS-home-call-drop
-bash homecall.sh fixmacos
