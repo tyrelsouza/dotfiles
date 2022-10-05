ebd24284a3e0863e0faa9d092029a68dd0df41b2
Tyrel Souza <tyrel@tyrelsouza.com>
2022-10-05 00:03:18 -0400

    change fish colors

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,5 +2,10 @@ if status is-interactive
     nitch
     set fish_greeting
     # Commands to run in interactive sessions can go here
+
+    set --universal hydro_color_pwd green
+    set --universal hydro_color_git red
+
+
     source ~/code/dotfiles/shell_funcs.fish
 end
