f55323d9265453b8587964ddb9611faf7a8edfb8
Tyrel Souza <tyrel@tyrelsouza.com>
2023-01-01 16:04:01 -0500

    fix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,22 @@
 {
   description = "My first nix flake";
 
-  inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
-      home-manager.url = "github:nix-community/home-manager";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
-      # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
-      darwin.url = "github:lnl7/nix-darwin";
-      darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
-  };
+inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
+};
 
-  outputs = { self, nixpkgs, home-manager, darwin }: {
-    darwinConfiguration."ts-tl-mbp" = darwin.lib.darwinSystem {
-      system = "x86_64-darwin";
-      modules = [
-        home-manager.darwinModules.home-manager
-        ./hosts/ts-tl-mbp/default.nix
-      ]; # will be important later
-    };
+outputs = { self, nixpkgs, home-manager, darwin }: {
+  darwinConfigurations."ts-tl-mbp" = darwin.lib.darwinSystem {
+  # you can have multiple darwinConfigurations per flake, one per hostname
+
+    system = "x86_64-darwin";
+    modules = [ home-manager.darwinModules.home-manager ./hosts/ts-tl-mbp/default.nix ]; # will be important later
   };
-  
+};
 
 }
--- hosts/ts-tl-mbp/default.nix
+++ hosts/ts-tl-mbp/default.nix
@@ -1,36 +1,38 @@
-# hosts/ts-tl-mbp/default.nix
 { pkgs, ... }:
 {
 
   # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
-
-  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  services.nix-daemon.package = pkgs.nixFlakes;
-  
-  # if you use zsh (the default on new macOS installations),
-  # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
-  programs.zsh.enable = true;
-
-  homebrew = {
-	  enable = true;
-	  autoUpdate = true;
-	  # updates homebrew packages on activation,
-	  # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
-	  casks = [
-	    "alfred"
-	    "discord"
-	  ];
-	};
-
-
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.users.tyrel = { pkgs, ... }: {
-  
-    stateVersion = "22.11"; # read below
-  
+programs.zsh.enable = true;
+system.defaults.dock.autohide = true;
+home-manager.useGlobalPkgs = true;
+home-manager.useUserPackages = true;
+home-manager.users.tyrel = { pkgs, ... }: {
+  home.stateVersion = "22.05";
+  programs.tmux = { # my tmux configuration, for example
+    enable = true;
+    keyMode = "vi";
+    clock24 = true;
+    historyLimit = 10000;
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
+      gruvbox
+    ];
+    extraConfig = ''
+      new-session -s main
+      bind-key -n C-a send-prefix
+    '';
   };
-
+};
+homebrew = {
+  enable = true;
+  autoUpdate = true;
+  # updates homebrew packages on activation,
+  # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
+  casks = [
+    "alfred"
+    "discord"
+  ];
+};
 
 }
