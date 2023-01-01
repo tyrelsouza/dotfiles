3b1475fefca4f16ff60d8037e949b5d906068e02
Tyrel Souza <tyrel@tyrelsouza.com>
2023-01-01 16:17:28 -0500

    blah

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
 {
-  description = "My first nix flake";
+	description = "Tyrel Nix";
 
 inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
+
 };
 
 outputs = { self, nixpkgs, home-manager, darwin }: {
   darwinConfigurations."ts-tl-mbp" = darwin.lib.darwinSystem {
-  # you can have multiple darwinConfigurations per flake, one per hostname
-
     system = "x86_64-darwin";
     modules = [ home-manager.darwinModules.home-manager ./hosts/ts-tl-mbp/default.nix ]; # will be important later
   };
