fa0e5d9bf732b57e3ff527945601eaa10aac24f7
Tyrel Souza <tyrel@tyrelsouza.com>
2023-01-02 13:36:27 -0500

    fish scripts

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # Commands to run in interactive sessions can go here
+
+    set --universal hydro_color_pwd green
+    set --universal hydro_color_git red
+
+    source ~/.config/fish/shell_funcs.fish
+    source ~/.config/fish/shell_aliases.fish
+    source ~/.config/fish/everquote.fish
 end
