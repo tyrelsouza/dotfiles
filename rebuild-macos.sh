ad75423f8161b503a30aa5b330fb1518f13ff1f7
Tyrel Souza <tyrelsouza@gmail.com>
2023-01-17 19:06:00 -0500

    cleanup scripts

--- rebuild-macos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-hostname=$(hostname| sed 's/.local//')
-./result/sw/bin/darwin-rebuild switch --flake ./hosts/$hostname
--- rebuild-ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NIX_PATH=nixpkgs=/nix/var/nix/profiles/per-user/tyrel/channels/nixpkgs:/nix/var/nix/profiles/per-user/tyrel/channels
-hostname=$(hostname)
-
-nix run ./hosts/$hostname switch -vv
-home-manager switch --flake ./hosts/$hostname
