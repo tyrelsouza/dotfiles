432a8fb491fd1559eaf5fae05952a4f247c6d904
Tyrel Souza <tyrelsouza@gmail.com>
2023-11-16 11:19:27 -0500

    more

--- setup.sh
+++ setup.sh
@@ -2,20 +2,42 @@
 
 HOME=/home/coder
 
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt update
-sudo apt install bzip2  libncurses5-dev libffi-dev libreadline-dev libssl-dev 
+sudo apt-get install -y libncurses5-dev neovim
+sudo apt-get install -y build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
 
+export MAKEDEB_RELEASE='makedeb'
+bash -c "$(wget -qO - 'https://shlink.makedeb.org/install')"
+
+curl https://sh.rustup.rs -sSf | sh
 
 sudo curl https://pyenv.run | bash
 
-ln -s ./home/config $HOME/.config
-ln -s ./home/bin $HOME/bin
-ln -s ./home/gitconfig $HOME/.gitconfig
-ln -s ./home/shell_aliases $HOME/.shell_aliases
-ln -s ./home/shell_funcs $HOME/.shell_funcs
-#ln -s home/zshrc $HOME/.zshrc
+mv ./home/config/nvim /home/coder/.config/nvim
+mv ./home/bin /home/coder/bin
+mv ./home/gitconfig /home/coder/.gitconfig
+mv ./home/shell_aliases /home/coder/.shell_aliases
+mv ./home/shell_funcs /home/coder/.shell_funcs
+#ln -s home/zshrc /home/coder/.zshrc
 #
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> /home/coder/.bashrc
+echo 'export PYENV_ROOT="/home/coder/.pyenv"' >> /home/coder/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/coder/.bashrc
 echo 'eval "$(pyenv init -)"' >> /home/coder/.bashrc
+source ~/.bashrc
 
+pyenv install 3.10.13
+pyenv global 3.10.13
+
+
+mkdir ~/.buildsrc
+pushd ~/.buildsrc
+
+git clone 'https://mpr.makedeb.org/just'
+cd just
+makedeb -si
+
+popd
+
+just vimpacker
+just pi
