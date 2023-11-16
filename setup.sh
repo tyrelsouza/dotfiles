64008ae9ba710306b0cc5575ff4d347bf60f283c
Tyrel Souza <tyrelsouza@gmail.com>
2023-11-16 00:25:12 -0500

    setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 HOME=/home/coder
 
 sudo apt update
+sudo apt install bzip2  libncurses5-dev libffi-dev libreadline-dev libssl-dev 
+
 
 sudo curl https://pyenv.run | bash
 
@@ -17,9 +19,3 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> /home/coder/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/coder/.bashrc
 echo 'eval "$(pyenv init -)"' >> /home/coder/.bashrc
 
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
-pyenv install 3.10
-pyenv global 3.10
